2187b4b1ec26ca62ffccf7b2b266f2be5c54bebd
sunny8212 <padariyasunny@gmail.com>
2025-02-03 15:44:25 +0530

    added product edit functionality

--- index.js
+++ index.js
@@ -331,6 +331,21 @@ app.get("/api/products", async (req, res) => {
 });
 
 
+// Get Products based on id
+  app.get("/api/products/:id", async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      res.json(product);
+    } catch (error) {
+      console.error("Error fetching product:", error);
+      res.status(500).json({ message: "Error fetching product", error: error.message });
+    }
+  });
+
+
 app.delete("/api/products/:id", async (req, res) => {
   try {
     console.log("Received delete request for ID:", req.params.id); // ✅ Log received ID
@@ -345,5 +360,33 @@ app.delete("/api/products/:id", async (req, res) => {
   } catch (error) {
     console.error("Error deleting product:", error);
     res.status(500).json({ message: "Error deleting product", error: error.message });
+  }
+});
+
+
+
+
+// Edit the product details
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, sku, category, quantity, price, supplier, status } = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, {
+      name,
+      sku,
+      category,
+      quantity,
+      price,
+      supplier,
+      status,
+    });
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json({ message: "Product updated successfully" });
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ message: "Error updating product", error: error.message });
   }
 });
